09df984721c431be1cb9bd5d48500194381a0a5d
Alina Chernyakevich <alishacher@gmail.com>
2020-04-27 19:56:27 +0300

    Added generation of data

---
+++ Husty-servise-agents/routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const reqlib = require('app-root-path').require;
+const logger = reqlib('logger');
+const os = require("os");
+const hostname = os.hostname();
+
+const util = require('util')
+const chance = require('chance')
+const faker = require('faker')
+
+const packageGenVersion = require('../lib/version.js') 
+const instance_id = process.env.NODE_APP_INSTANCE || 0 
+
+
+/* GET home page. */
+router.get('/', (req, res) => {
+	res.render('index', { company: "BSU", year: 2020 });
+
+  
+});
+
+module.exports = router;
