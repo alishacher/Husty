6f3b1c560c702be8dde9264948b45a50b2aebc8b
Alina Chernyakevich <alishacher@gmail.com>
2020-05-01 17:24:16 +0300

    Tested Api service

--- Husty-service-orders/docker-yandextank.sh
+++ Husty-service-orders/docker-yandextank.sh
@@ -1,2 +1,3 @@
-docker run --rm  --link fp-orders:targethost -v $(pwd):/var/loadtest -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent -v $HOME/.ssh:/root/.ssh --net app-tier -it direvius/yandex-tank
+#docker run --rm  --link husty-service-orders_node_1:targethost -v $(pwd):/var/loadtest -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent -v $HOME/.ssh:/root/.ssh -it direvius/yandex-tank
 
+docker run --rm  --net host -v $(pwd):/var/loadtest -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent -v $HOME/.ssh:/root/.ssh -it direvius/yandex-tank
